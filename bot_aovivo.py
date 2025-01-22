5cc7bd7b6d25f517b57389f31c2e33bd2dfc8685
novaisnivaldo <nnovais.nivaldo@gmail.com>
2025-01-21 23:00:48 -0300

    Update bot_aovivo.py

--- bot_aovivo.py
+++ bot_aovivo.py
@@ -8,20 +8,54 @@ sessao = 'Repassagem Mensagem'
 def main():
         print ('Monitoriamento iniciado...')
         client = TelegramClient (sessao, api_id, api_hash)
-        @client.on(events.NewMessage(chats = [4506407899]))
+        tabela_size = 100
+        tabela_bot = []
+        @client.on(events.NewMessage(chats = [1002356837091]))
         async def enviar_mensagem(event):
-            # Mensagem original
-            mensagem = event.raw_text
-            # Dividir a mensagem em linhas
-            linhas = mensagem.strip().split('\n')
-            # Selecionar as linhas desejadas (1, 3, 4 e 5)
-            linhas_filtradas = [linhas[0], linhas[2], linhas[3], linhas[4], linhas[5]]
-            # Juntar as linhas filtradas de volta em uma string
+            msg_orig = event.raw_text
+            linhas = msg_orig.strip().split('\n')
+            linhas_filtradas = [linhas[0], linhas[1], linhas[2], linhas[3], linhas[4]]
             mensagem_filtrada = '\n'.join(linhas_filtradas)
-            print(mensagem_filtrada)
-            #print(mensagem)
-            #print(event.raw_text)
-            await client.send_message(-1002481733456,mensagem_filtrada)
+
+            def remove_linhas_com_palavras(msg_orig, palavras):
+                linhas = msg_orig.splitlines()
+                linhas_excluidas = [
+                    linha for linha in linhas
+                    if not any(palavra.lower() in linha.lower() for palavra in palavras)
+                ]
+                return "\n".join(linhas_excluidas)
+            texto = msg_orig
+            palavras = ["robô", "Bug","green365","Estatísticas"]
+            texto_filtrado = remove_linhas_com_palavras(texto, palavras)
+
+            def processar_mensagem(msg_orig):
+                 if "Relatório Diário" in msg_orig:
+                      return texto_filtrado
+                 else:
+                      return mensagem_filtrada
+            msg_replace = processar_mensagem(msg_orig)
+            msg_send = msg_replace.replace("📈", "🎮 ").replace("🕓","⏰ ").replace("🅿️", "⚽️ ").replace("🥇","🏆 ").replace("🏆Tip:","Resultado:")
+            msg_copy = await client.send_message(-1002481733456,msg_send)
+            id_msg_copy = msg_copy.id
+            id_msg_orig = event.message.id
+            if len(tabela_bot) >= tabela_size:
+                tabela_bot.pop(0)
+            tabela_bot.append([id_msg_orig,id_msg_copy])
+
+        @client.on(events.MessageEdited(chats = [1002356837091]))
+        async def enviar_mensagem(event):
+            msg_editada = event.raw_text
+            linhas = msg_editada.strip().split('\n')
+            linhas_filtradas = [linhas[0], linhas[1], linhas[2], linhas[3], linhas[4], linhas[5], linhas[1], linhas[27]]
+            msg_filtrada = '\n'.join(linhas_filtradas)
+            msg_replace = msg_filtrada.replace("📈", "🎮 ").replace("🕓","⏰ ").replace("🅿️", "⚽️ ").replace("🥇","🏆 ").replace("🏆Tip:","Resultado:")
+            msg_alt = event.message.id
+            for i in range(len(tabela_bot)):
+                if msg_alt == tabela_bot[i][0]:
+                    msg_edit = tabela_bot[i][1]
+                    break
+            await client.edit_message(-1002481733456,msg_edit,msg_replace)
+
         client.start()
         client.run_until_disconnected()
 main()
